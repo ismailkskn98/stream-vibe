e3127a9d7215a17862c2e7f08b6d815db0cc3284
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-12-04 22:22:14 +0300

    css hataları giderildi

--- src/components/categoriesCarousel/carousel/mobileProgressBar.tsx
+++ src/components/categoriesCarousel/carousel/mobileProgressBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function MobileProgressBar({ progress }: { progress: number }) {
   return (
-    <article className="w-full flex-center">
+    <article className="w-full flex items-center justify-center">
       <div className="flex lg:hidden w-1/4 h-1 bg-black-10 overflow-hidden">
         <div className="h-full bg-red-45 transition-all duration-300 ease-out" style={{ width: `${progress * 100}%` }} />
       </div>

68efebdfefb05f622a1b8eb03aa74319771cb3f4
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-12-15 19:17:43 +0300

    navigationBar Icon düzenledi

--- src/components/common/header/buttons.tsx
+++ src/components/common/header/buttons.tsx
@@ -5,7 +5,7 @@ import { IoNotificationsOutline } from "react-icons/io5";
 export default function Buttons() {
   return (
     <div className="hidden lg:flex items-center gap-8 text-white ">
-      <GoSearch className="w-7 h-7 hover:text-grey-75 transition-all cursor-pointer" aria-label="Search" />
+      <GoSearch className="w-9 h-9 hover:text-grey-75 transition-all cursor-pointer" aria-label="Search" />
       <IoNotificationsOutline className="w-7 h-7 hover:text-grey-75 transition-all cursor-pointer" aria-label="Notifications" />
     </div>
   );

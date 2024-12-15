b8ea9ea729eaed1a4c30474e4d8633244c8c4a6e
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-12-15 19:16:11 +0300

    logo kalitesi yükseltildi

--- src/components/common/header/logo.tsx
+++ src/components/common/header/logo.tsx
@@ -6,7 +6,7 @@ import logoImage from "@/../public/logo.png";
 export default function Logo() {
   return (
     <Link href="/" className="relative w-48 h-14">
-      <Image src={logoImage} alt="StreamVibe" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
+      <Image src={logoImage} quality={100} alt="StreamVibe" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
     </Link>
   );
 }
